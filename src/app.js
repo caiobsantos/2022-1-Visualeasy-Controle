66fe042f8f6aa4dc74d1f045b249e4fdb3fd1d74
Caio Santos <caiobs10@gmail.com>
2023-02-08 10:46:04 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -21,6 +21,15 @@ let corsOptions = {
     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
+app.use((req, res, next) => {
+	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+    res.header("Access-Control-Allow-Origin", "*");
+	//Quais são os métodos que a conexão pode realizar na API
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    app.use(cors());
+    next();
+});
+
 app.use(helmet.hidePoweredBy());
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
